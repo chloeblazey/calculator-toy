1da9b3444dd6ded3e25455e8032b68eec0c24812
chloeblazey <chloeblazey@gmail.com>
2021-03-07 11:38:14 -0800

    manages stack and current entry

--- src/components/MyCalculator.vue
+++ src/components/MyCalculator.vue
@@ -2,55 +2,71 @@
   <div class="myCalculator">
 
     <!--  display interface  -->
-    <h1>{{lastEntry}}</h1>
-    <h2>{{entryLog}}</h2>
+    <div class="displayInterface">
+      <!-- <p>{{lastEntry}}</p><br> -->
+      <p>{{currentEntry}}</p>
+    </div>
+    
     <!--  button interface  -->
     <div class="buttonInterface">
-      <button value="+" @click="handleButton">+</button>
-      <button value="*" @click="handleButton">*</button>
-      <button value="-" @click="handleButton">-</button>
-      <button value="/" @click="handleButton">/</button>
-      <button value="7" @click="handleButton">7</button>
-      <button value="8" @click="handleButton">8</button>
-      <button value="9" @click="handleButton">9</button>
+      <button value="+" @click="operatorButton">+</button>
+      <button value="*" @click="operatorButton">*</button>
+      <button value="-" @click="operatorButton">-</button>
+      <button value="/" @click="operatorButton">/</button>
+      <button value="7" @click="digitButton">7</button>
+      <button value="8" @click="digitButton">8</button>
+      <button value="9" @click="digitButton">9</button>
       <button value="clear" @click="allClear">AC</button>
-      <button value="4" @click="handleButton">4</button>
-      <button value="5" @click="handleButton">5</button>
-      <button value="6" @click="handleButton">6</button>
+      <button value="4" @click="digitButton">4</button>
+      <button value="5" @click="digitButton">5</button>
+      <button value="6" @click="digitButton">6</button>
       <button value="delete" @click="clearEntry">CE</button>
-      <button value="1" @click="handleButton">1</button>
-      <button value="3" @click="handleButton">3</button>
-      <button value="2" @click="handleButton">2</button>
+      <button value="1" @click="digitButton">1</button>
+      <button value="3" @click="digitButton">3</button>
+      <button value="2" @click="digitButton">2</button>
       <button class="button enter">=</button>
       <button value="0"
-              class="button zero" @click="handleButton">0</button>
+              class="button zero" @click="digitButton">0</button>
       <button value=".">.</button>
     </div>
   </div>
 </template>
 
 <script>
-// Main calculator controller
-
 export default {
   name: 'MyCalculator',
   data() {
     return {
-      lastEntry: '',
-      entryLog: ''
+      currentEntry: '',
+      entryStack: [],
+      currentIsOperator: false
     }
   },
   methods: {
-    handleButton(e) {
-      this.lastEntry = e.target.value
-      this.entryLog += e.target.value
+    digitButton(e) {
+      if (this.currentIsOperator) {
+        this.entryStack.push(this.currentEntry)
+        this.currentEntry = e.target.value
+        this.currentIsOperator = false
+      } else {
+        this.currentEntry += e.target.value
+      }
     },
     allClear() {
-      this.entryLog = ''
-      this.lastEntry = ''
+      this.currentEntry = ''
+      this.entryStack = []
     },
     clearEntry() {
-      this.lastEntry = ''
+      this.currentEntry = ''
+    },
+    operatorButton(e) {
+      if (this.currentIsOperator) {
+        this.currentEntry = e.target.value
+      } else {
+        this.entryStack.push(this.currentEntry)
+        this.currentEntry = e.target.value
+        this.currentIsOperator = true 
+      }
     }
   }
 }
@@ -68,6 +84,11 @@ export default {
     grid-template-rows: repeat(5, minmax(7rem, auto));
     margin: auto;
     width: 100%;
+  }
+
+  .displayInterface p{
+    display: inline-block;
+    height: 3rem;
   }
 </style>
   
